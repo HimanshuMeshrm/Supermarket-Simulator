20f0ec5ba9aa42fb165c72cbd8f6a1b774ce74cb
HimanshuMeshrm <hdmeshram006@gmail.com>
2025-04-29 11:45:17 +0530

    0.2
    Assets

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(h, 0f, v);
+        transform.Translate(move * moveSpeed * Time.deltaTime, Space.World);
+    }
+}
+
+
+
+
+
